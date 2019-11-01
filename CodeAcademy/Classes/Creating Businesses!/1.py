488fb40b939120c8350304f5a12910ced33fe1e2
umair-zohaib <uzumair4@gmail.com>
2019-11-01 19:24:50 +0000

    classes

---
+++ CodeAcademy/Classes/Creating Businesses!/1.py
@@ -0,0 +1,84 @@
+class Business:
+  def __init__(self, name,franchises ):
+    self.name=name
+    self.franchises=franchises
+  def __repr__(self):
+    return self.address
+
+class Franchise:
+  def __init__(self, address,menus ):
+    self.address=address
+    self.menus=menus
+  def __repr__(self):
+    return self.address
+  
+  def avail_menus(self , time):
+    avail_menus=[]
+    for i in self.menus:
+      if ( time >= self.start_time and time <= self.end_time ):
+        avail_menus.append(i)
+    return avail_menu
+  
+class Menu:
+  def __init__(self, name,items, start_time ,end_time  ):
+    self.name=name
+    self.items=items
+    self.start_time=start_time
+    self.end_time=end_time 
+    
+  def __repr__(self):
+    return self.name + " from " + str(self.start_time) + " to "+ str(self.end_time)
+  
+  def calculate_bill(self , purchased_items):
+    t_bill = 0
+    for i in purchased_items:
+      if i in purchased_items:
+        t_bill += self.items[i]
+    
+    return t_bill
+    
+    
+      
+    
+brunch_items = {
+  'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00, 'home fries': 4.50, 'coffee': 1.50, 'espresso': 3.00, 'tea': 1.00, 'mimosa': 10.50, 'orange juice': 3.50
+}
+
+brunch_menu = Menu('Brunch' , brunch_items , 1100 , 1600)
+
+early_bird_items = {
+  'salumeria plate': 8.00, 'salad and breadsticks (serves 2, no refills)': 14.00, 'pizza with quattro formaggi': 9.00, 'duck ragu': 17.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 1.50, 'espresso': 3.00,
+}
+
+
+early_bird_menu = Menu('Early_Bird' , early_bird_items , 1500 , 1800)
+
+dinner_items = {
+  'crostini with eggplant caponata': 13.00, 'ceaser salad': 16.00, 'pizza with quattro formaggi': 11.00, 'duck ragu': 19.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 2.00, 'espresso': 3.00,
+}
+
+
+dinner_menu = Menu('Dinner' , dinner_items , 1700 , 2300)
+
+
+kids_items = {
+  'chicken nuggets': 6.50, 'fusilli with wild mushrooms': 12.00, 'apple juice': 3.00
+}
+
+kids_menu = Menu('Kids' , kids_items , 1100 , 2100)
+#print (brunch_menu.calculate_bill(['pancakes' , 'waffles', 'burger'] ))
+
+
+arepas_items= {
+  'arepa pabellon': 7.00, 'pernil arepa': 8.50, 'guayanes arepa': 8.00, 'jamon arepa': 7.50
+}
+
+arepas_menu = Menu('Arepa' , arepas_items , 1000 ,  2000)
+
+arepas_place = Franchise("189 Fitzgerald Avenu" , [arepas_menu])
+
+arepa = Business("Take a' Arepa" , [arepas_place])
+
+
+
+
---
+++ CodeAcademy/Classes/Learn Python: Inheritance and Polymorphism/8.py
@@ -0,0 +1,12 @@
+class LawFirm:
+  def __init__(self, practice, lawyers):
+    self.practice = practice
+    self.lawyers = lawyers
+    
+  def __len__(self):
+    return len(self.lawyers)
+  
+  def __contains__(self, lawyer):
+    return lawyer in self.lawyers
+    
+d_and_p = LawFirm("Injury", ["Donelli", "Paderewski"])
